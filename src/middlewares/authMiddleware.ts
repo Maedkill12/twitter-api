2a3bfa0db81b51430061dc7be087da4ec1686878
maed <maed012014@gmail.com>
2023-05-12 17:56:02 -0600

    JWT_SECRET env

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,7 +3,9 @@ import { Request, Response, NextFunction } from "express";
 import { PrismaClient, User } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const JWT_SECRET = "SUPER SECRET";
+const JWT_SECRET = process.env.JWT_SECRET || "SUPER SECRET";
+
+console.log(JWT_SECRET);
 
 type AuthRequest = Request & { user?: User };
 
